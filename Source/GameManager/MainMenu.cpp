0baaca2c9b56a14f2dd8561c794149f1a94a0720
Niall Murray <npmurray.nic.edu>
2024-11-24 23:17:29 -0800

    background

--- Source/GameManager/MainMenu.cpp
+++ Source/GameManager/MainMenu.cpp
@@ -15,8 +15,12 @@
  */
 MainMenu::MainMenu(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys) : State(window, supportedKeys) {
     initializeFonts();
+    initializeTextures();
 
     background.setSize(sf::Vector2f(window->getSize().x, window->getSize().y));
+    background.setPosition(0, 0);
+
+    background.setTexture(&textures["BACKGROUND"]);
 
     // play button
     playButton = new Button(fonts["SONO_R"], "PLAY", sf::Vector2f(window->getSize().x / 6, window->getSize().y / 8), sf::Color(70, 70, 70, 150), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200));
@@ -78,6 +82,7 @@ void MainMenu::render(sf::RenderTarget* target) {
     if(!target)
         target = window;
 
+    target->draw(background);
     playButton->render(*target);
     exitButton->render(*target);
 }
