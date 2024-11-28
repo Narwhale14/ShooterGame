3f128eb75ebb801a8d9c34e5cf8ae5fc749f0996
William Wotherspoon <wtwotherspoon@nic.edu>
2024-11-28 02:36:21 -0800

    fixed final up

--- Resource/Entity/LevelBar.h
+++ Resource/Entity/LevelBar.h
@@ -38,7 +38,7 @@ class LevelBar {
         bool addXp(int incoming);
 
         void render(sf::RenderTarget& target);
-        int levelCap;
+        
     private:
         int level;
         int xp;
@@ -46,7 +46,7 @@ class LevelBar {
         int requiredXpToLevelUp;
         float maxMainBarSize;
         float levelRatio;
-        
+        int levelCap;
 
         sf::RectangleShape barBack;
         sf::RectangleShape barMain;
